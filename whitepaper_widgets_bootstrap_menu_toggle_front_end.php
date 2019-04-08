d051b7e51d603aa2a40fa6091318d70176fc9c00
Dillon Brickhouse <trombonecricket@gmail.com>
2019-04-08 07:15:14 -0600

    Replaced Bootstrap menu toggle bars with FontAwesome

--- whitepaper_widgets_bootstrap_menu_toggle_front_end.php
+++ whitepaper_widgets_bootstrap_menu_toggle_front_end.php
@@ -1,5 +1,5 @@
 <div class="navbar-toggle-button <?php echo $toggle_class; ?>">
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
+    <i class="fas fa-bars"></i>
   </button>
 </div>
